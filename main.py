b2aa41c9cfb40ca5f999b6c778c5bd5ff799edef
wikomiks <97792998+wikomiks@users.noreply.github.com>
2022-08-17 16:17:58 +0200

    working raw runner

---
+++ main.py
@@ -0,0 +1,8 @@
+import os
+try: import requests
+except: os.system("pip install requests") 
+try:
+    raw=requests.get(input("Enter raw link: ")).content
+except: print("That website doesn't exist or has error.")
+exec(raw)
+input()
